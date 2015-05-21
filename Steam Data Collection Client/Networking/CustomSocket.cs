9253e609c39b632a2ee77cab0e141f757d3d2932
Jack Moorhouse <moverperfect@gmail.com>
2015-05-22 00:23:31 +0100

    Changed the synchronus socket to allow packets with 'unlimited' size

--- Steam Data Collection Client/Networking/CustomSocket.cs
+++ Steam Data Collection Client/Networking/CustomSocket.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Windows.Forms.VisualStyles;
 
 namespace Steam_Data_Collection_Client.Networking
 {
@@ -15,7 +16,7 @@ namespace Steam_Data_Collection_Client.Networking
             while (true)
             {
                 // Data buffer for incoming data.
-                var bytes = new byte[1024];
+                var buffer = new byte[2];
 
                 // Connect to a remote device.
                 try
@@ -41,22 +42,40 @@ namespace Steam_Data_Collection_Client.Networking
                         var bytesSent = sender.Send(msg);
 
                         sender.ReceiveTimeout = 5000;
+                        sender.ReceiveBufferSize = 2;
+                        var bytesRec = sender.Receive(buffer, 2, SocketFlags.None);
+
+                        sender.ReceiveBufferSize = 8192;
+
+                        while (buffer.Length != BitConverter.ToUInt16(buffer,0))
+                        {
+                            if ((BitConverter.ToUInt16(buffer, 0) - buffer.Length) < 8192)
+                            {
+                                var length = buffer.Length;
+                                Array.Resize(ref buffer, BitConverter.ToUInt16(buffer, 0));
+                                sender.Receive(buffer, length, buffer.Length - length, SocketFlags.None);
+                            }
+                            else
+                            {
+                                var length = buffer.Length;
+                                Array.Resize(ref buffer, buffer.Length + 8192);
+                                sender.Receive(buffer, length, 8192, SocketFlags.None);
+                            }
+                        }
 
                         // Receive the response from the remote device.
-                        var bytesRec = sender.Receive(bytes);
+                        //bytesRec = sender.Receive(buffer);
 
                         // Release the socket.
                         sender.Shutdown(SocketShutdown.Both);
                         sender.Close();
 
-                        Array.Resize(ref bytes, bytesRec);
-
-                        if (bytesRec < 8)
+                        if (buffer.Length < 8)
                         {
                             throw new Exception();
                         }
 
-                        return bytes;
+                        return buffer;
                     }
                     catch (ArgumentNullException ane)
                     {
