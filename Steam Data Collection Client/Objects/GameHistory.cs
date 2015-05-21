0f271ff5dd16891d645bee0663ceb355da5d1cdc
Jack Moorhouse <moverperfect@gmail.com>
2015-05-22 00:24:24 +0100

    Added the game name to the GameHistory object

--- Steam Data Collection Client/Objects/GameHistory.cs
+++ Steam Data Collection Client/Objects/GameHistory.cs
@@ -10,17 +10,22 @@ namespace Steam_Data_Collection_Client.Objects
         /// <summary>
         /// The ID number of the app
         /// </summary>
-        public int AppId { get; internal set; }
+        public int AppId { get; set; }
 
         /// <summary>
-        /// The number
+        /// The number of time in the last 2 weeks
         /// </summary>
         public int Last2Weeks { get; set; }
 
         /// <summary>
-        /// 
+        /// Time spent in total
         /// </summary>
         public int OnRecord { get; set; }
+
+        /// <summary>
+        /// The name of the game
+        /// </summary>
+        public string Name { get; set; }
 
         #endregion
     }
